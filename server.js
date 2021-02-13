ad9938307eb02af1c8edcfb1e6e064eba9fae7b2
niraj-khatiwada <nirazzz.nk@gmail.com>
2021-02-14 01:35:37 +0545

    Graphql-Constraints-Directive having package format error issu

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { ApolloServer, makeExecutableSchema } = require('apollo-server-express')
+const { ApolloServer } = require('apollo-server-express')
+const { makeExecutableSchema } = require('graphql-tools')
 const dotenv = require('dotenv')
+const {
+  constraintDirective,
+  constraintDirectiveTypeDefs,
+} = require('graphql-constraint-directive')
 
 const db = require('./src/models/index')
+const { formatError } = require('./src/tools/error')
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.SERVER_PORT
@@ -11,15 +17,23 @@ dotenv.config()
 
 const app = express()
 
-app.use(bodyParser.json())
-
 const typeDefs = require('./src/schema/index')
 const resolvers = require('./src/resolvers/index')
-const schema = makeExecutableSchema({ typeDefs, resolvers })
+const schema = makeExecutableSchema({
+  typeDefs: [...typeDefs, constraintDirectiveTypeDefs],
+  resolvers,
+  schemaTransforms: [constraintDirective()],
+})
 
-const apolloServer = new ApolloServer({ schema })
+const apolloServer = new ApolloServer({
+  schema,
+  context: { db },
+  formatError,
+})
 
-apolloServer.applyMiddleware({ app: app, path: '/graphql' })
+apolloServer.applyMiddleware({ app: app })
+
+app.use('/graphql', bodyParser.json())
 
 db.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
